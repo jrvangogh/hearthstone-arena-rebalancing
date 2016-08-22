3f3e6c050f9252e9f9798c9bcfeff5f7d1911afb
jrvangogh <jrvangogh@gmail.com>
2016-08-22 15:11:05 -0700

    Separate Classes
    Create data frames for each class

--- save_json.py
+++ save_json.py
@@ -7,7 +7,7 @@ Reads Hearthstone card data in as a JSON file and saves it to a CSV
 @author: Jacob
 """
 
-#import pandas as pd
+#%% Import and Set Constants
 import json as js
 import urllib.request as urllib
 import pandas as pd
@@ -57,14 +57,27 @@ arena_dataframe.to_csv(ARENA_OUT_FILE)
 page = requests.get(CARD_ARENA_SCORES_URL)
 tree = html.fromstring(page.content)
 
-class_sections = []
+score_dict = {c:{} for c in CLASSES}
 for hs_class in CLASSES:
-    xpath_string = '//section[@id=' + hs_class + ']'
-    class_sections.append(tree.xpath(xpath_string))
+    d = score_dict[hs_class]
+    xpath_string = '//section[@id="' + hs_class + '"]'
+    section = tree.xpath(xpath_string)[0]
+    
+    for rarity in section.xpath('ul/li'):
+        for tier in rarity.xpath('ul/li'):
+            for card in tier.xpath('ol/li'):
+                name = card.xpath('dl/dt')[0].text.strip()
+                if name:
+                    score = int(card.xpath('dl/dd')[0].text)
+                    d[name] = score
 
 
-
-
-
-
-
+#%% Separate Arena Data By Class
+df_dict = {c:{} for c in CLASSES}
+neutral = arena_dataframe[arena_dataframe['playerClass'] == 'NEUTRAL']
+for hs_class in CLASSES:
+    class_df = arena_dataframe[arena_dataframe['playerClass'] == hs_class.upper()]
+    class_df = class_df.append(neutral)
+    class_df['arenaScoreClass'] = hs_class.upper()
+    df_dict[hs_class] = class_df
+    
