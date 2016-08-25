95ad2eb35abaa53331881314850e9180c9f3bd98
jrvangogh <jrvangogh@gmail.com>
2016-08-25 13:15:22 -0700

    Created methods for simulating a draft
    Methods for simulating drafting the best card every time

--- hs_arena_kit.py
+++ hs_arena_kit.py
@@ -8,8 +8,9 @@ Created on Wed Aug 24 22:09:08 2016
 import numpy as np
 import random
 from bisect import bisect
+import pandas as pd
 
-class ArenaDraftCalculator:
+class ArenaDraftSimulator:
     def __init__(self, card_weights=None, card_names=None, card_scores=None,
                  card_tuples=None, card_df=None, df_cols=None):
         """
@@ -43,6 +44,7 @@ class ArenaDraftCalculator:
         
         self.cum_weights = list(np.cumsum(self.weights))
         self.total_weight = self.cum_weights[-1]
+        self.draft = None
 
 
     def offerCard(self):
@@ -53,9 +55,52 @@ class ArenaDraftCalculator:
         r = random.random() * self.total_weight
         i = bisect(self.cum_weights, r)
         return( (self.scores[i], self.names[i]) )
-    
+
+
     def offerCards(self, num_iter):
         """
         Randomly selects multiple cards
         """
         return ([self.offerCard() for c in range(num_iter)])
+
+
+    def draftCard(self, max_only=True):
+        """
+        Randomly selects 3 cards and returns the one
+        with the highest arena score
+        """
+        return max([self.offerCards(3)])
+
+
+    def draftCards(self, num_iter, as_DF=False):
+        """
+        Randomly drafts multiple cards and stores this draft
+        for stats
+        """
+        self.draft = [self.draftCard() for c in range(num_iter)]
+        return self.getDraft(as_DF)
+
+    
+    def getDraft(self, as_DF=False):
+        """
+        Returns the draft of cards
+        """
+        if as_DF:
+            return pd.DataFrame(self.draft, columns=['score','name'])
+        return self.draft
+
+
+    def getDraftStats(self):
+        """
+        Returns a stastical description of the scores of the draft
+        """
+        return (self.getDraft(as_DF=True)['score'].describe())
+
+
+
+
+
+
+
+
+
