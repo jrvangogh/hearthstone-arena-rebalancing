f14d9a978441c4dd543d232555a14191812afaf3
jrvangogh <jrvangogh@gmail.com>
2016-08-25 15:43:08 -0700

    Created method for calculating offer rates
    Method takes a data frame and adds a column, "weight". This column
    stores the probability of a particular card being offered under standard
    arena draft rules.

--- hs_arena_kit.py
+++ hs_arena_kit.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 from bisect import bisect
 import pandas as pd
+import warnings
 
 class ArenaDraftSimulator:
     def __init__(self, card_weights=None, card_names=None, card_scores=None,
@@ -99,9 +100,70 @@ class ArenaDraftSimulator:
 
 #%% Methods for Calculating Weights of a Standard Arena Draft
 
+CLASSES = ['DRUID', 'HUNTER', 'MAGE', 'PALADIN', 'PRIEST', 
+           'ROGUE', 'SHAMAN', 'WARLOCK', 'WARRIOR']
+RARITY_RATES = {'COMMON': 76, 'RARE': 20, 'EPIC': 3, 'LEGENDARY': 1}
+CLASS_RATES = {True: (2/3), False: (1/3)}
 
-
-
-
-
-
+def add_standard_offering_rates(df):
+    """
+    Given a data frame of arena cards, adds a column that
+    contains the probabilities of each card being offered
+    Necessary columns:
+    arenaScoreClass - value of each class arena set
+        * values - each class in all caps
+    playerClass - value of the card's class set
+        * values - each class and 'neutral' in all caps
+    rarity - rarity of each card
+        * values - 'common', 'rare', 'epic', 'legendary' in all caps
+    """
+    # Silence warnings due to pandas bug
+    warnings.simplefilter(action = "ignore", category = RuntimeWarning)
+    
+    # For every class
+    for c in CLASSES:
+        # For every rarity
+        for r in RARITY_RATES:
+            # Slice the data frame for a particular arena set and rarity
+            if r == 'COMMON':
+                f = df[(df['arenaScoreClass'] == c) & \
+                       ((df['rarity'] == r) | (df['rarity'] == 'FREE'))]
+            else:
+                f = df[(df['arenaScoreClass'] == c) & (df['rarity'] == r)]
+            
+            # Count the number of cards based on rarities and class
+            full_count = len(f)
+            class_count = len(f[f['playerClass'] == c])
+            neutral_count = full_count - class_count
+            
+            # Calculate the weight of the cards
+            class_w = RARITY_RATES[r] * CLASS_RATES[True] / class_count
+            neut_w = RARITY_RATES[r] * CLASS_RATES[False] / neutral_count
+            print(c, r)
+            print(full_count, class_count, neutral_count)
+            print(class_w, neut_w)
+            print(class_count * class_w, neutral_count * neut_w, class_count * class_w + neutral_count * neut_w)
+            print('')
+            
+            # Set the cards' weights
+            if r == 'COMMON':
+                df.set_value((df['arenaScoreClass'] == c) & \
+                             (df['playerClass'] == c) & \
+                             ((df['rarity'] == r) | (df['rarity'] == 'FREE')),
+                             'weight', class_w)
+                         
+                df.set_value((df['arenaScoreClass'] == c)& \
+                             (df['playerClass'] == 'NEUTRAL') & \
+                             ((df['rarity'] == r) | (df['rarity'] == 'FREE')),
+                             'weight', neut_w)
+            else:
+                df.set_value((df['arenaScoreClass'] == c) & \
+                             (df['playerClass'] == c) & \
+                             (df['rarity'] == r),
+                             'weight', class_w)
+                         
+                df.set_value((df['arenaScoreClass'] == c)& \
+                             (df['playerClass'] == 'NEUTRAL') & \
+                             (df['rarity'] == r),
+                             'weight', neut_w)
+    return df
