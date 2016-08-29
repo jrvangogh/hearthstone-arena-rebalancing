d519f4007a8cffdeb84ad31510b6f59f21336319
jrvangogh <jrvangogh@gmail.com>
2016-08-29 14:22:16 -0700

    Added script for doing data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -6,4 +6,17 @@ Created on Fri Aug 26 15:19:40 2016
 """
 
 import pandas as pd
+import statistics as stat
 import hs_arena_kit as hak
+
+DATA_CSV = "arena_data.csv"
+STATS_CSV = "stats.csv"
+
+data = pd.read_csv("arena_data.csv")
+classes = list(data["arenaScoreClass"].unique())
+
+iterations = 10
+score_median = stat.median(data[data['arenaScoreClass'] == 'SHAMAN']['arenaScore'])
+
+#%% Add New Columns for Different Weight Formulas
+hak.add_all_weights(df=data, center_val=score_median)
