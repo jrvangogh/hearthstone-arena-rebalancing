4f5499d13dd6a8b2c05df85e3849771afca8d58a
jrvangogh <jrvangogh@gmail.com>
2016-08-29 18:30:31 -0700

    data_analysis script now simulates drafts on different weights
    Each weight formula drafts a number of cards equivalent to 10,000 decks.

--- data_analysis.py
+++ data_analysis.py
@@ -15,8 +15,28 @@ STATS_CSV = "stats.csv"
 data = pd.read_csv("arena_data.csv")
 classes = list(data["arenaScoreClass"].unique())
 
-iterations = 10
 score_median = stat.median(data[data['arenaScoreClass'] == 'SHAMAN']['arenaScore'])
 
 #%% Add New Columns for Different Weight Formulas
 hak.add_all_weights(df=data, center_val=score_median)
+data.to_csv(DATA_CSV, index=False)
+
+
+#%% Gather Draft Data for Different Weight Formulas
+stats = []
+for c in hak.CLASSES:
+    f = data[data['arenaScoreClass'] == c]
+    for formula, col_name in hak.DEFAULT_WEIGHT_FORMULAS.items():
+        cols = [col_name, 'name', 'arenaScore']
+        ads = hak.ArenaDraftSimulator(card_df=f, df_cols=cols)
+        ads.draft_cards(num_iter=iterations)
+        stat = ads.get_draft_stats()
+        stat['class'] = c
+        stat['formula'] = formula
+        stats.append(stat.transpose())
+stats = pd.concat(stats, axis=1).transpose()
+cols = ['formula', 'class', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+stats = stats[cols]
+stats = stats.sort(columns=['formula', 'class'])
+stats = stats.reset_index()
+stats.to_csv(STATS_CSV, index=False)
